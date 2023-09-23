0749aec5ca1146eb439e5c2413f2e55a5f95a157
Byron Wang <byronwang123919@gmail.com>
2023-09-23 13:07:44 -0700

    more initialization

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
-import { Box, Button, Text, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
+import Homescreen from "./components/Homescreen";
 
 function App() {
   return (
-    <VStack bgColor="green.300">
-      <Text>Hello world</Text>
-      <Button>This is a button</Button>
+    <VStack>
+      <Homescreen />
     </VStack>
   );
 }
