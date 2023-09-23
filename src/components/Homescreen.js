b05b4b2f8f3213078016a6661199586c510ca774
Byron Wang <byronwang123919@gmail.com>
2023-09-23 14:44:48 -0700

    finished upload file functionality

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,8 +1,31 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Input } from "@chakra-ui/react";
 import React from "react";
+import { readFile } from "../helpers/helper";
 
 const Homescreen = () => {
-  return <Box>Homescreen</Box>;
+  const handleFileUpload = (event) => {
+    readFile(event);
+  };
+
+  return (
+    <Box>
+      <Button
+        _hover={{
+          cursor: "pointer",
+        }}
+        as="label"
+        htmlFor="fileInput"
+      >
+        Upload file
+      </Button>
+      <Input
+        type="file"
+        id="fileInput"
+        display="none"
+        onChange={handleFileUpload}
+      />
+    </Box>
+  );
 };
 
 export default Homescreen;
