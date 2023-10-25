30aaac431ff398856a708d2e4f4627f69e39ae64
Byron Wang <byronwang123919@gmail.com>
2023-10-25 00:11:01 -0700

    selecting the days and the hours of the days for the event:

---
+++ src/components/DayHours.js
@@ -0,0 +1,88 @@
+import { Box, Button, Input, Text, VStack } from "@chakra-ui/react";
+import React, { useContext, useEffect, useState } from "react";
+import { DataContext } from "../App";
+
+const DayHours = ({ onPrev, onNext }) => {
+  const { data, setData } = useContext(DataContext);
+  const [days, setDays] = useState([]);
+  const [hours, setHours] = useState({});
+
+  useEffect(() => {
+    setDays(data.daysList);
+  }, [data]);
+
+  useEffect(() => {
+    console.log(hours, " ius the hours");
+  }, [hours]);
+
+  useEffect(() => {
+    console.log(days, " is days");
+    for (let i = 0; i < days.length; i++) {
+      const currDay = days[i];
+      hours[currDay] = {
+        start: "08:00",
+        end: "18:00",
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [days]);
+
+  const handleStartTimeChange = (day, value) => {
+    setHours((prevHours) => ({
+      ...prevHours,
+      [day]: { ...prevHours[day], start: value },
+    }));
+  };
+
+  const handleEndTimeChange = (day, value) => {
+    setHours((prevHours) => ({
+      ...prevHours,
+      [day]: { ...prevHours[day], end: value },
+    }));
+  };
+
+  return (
+    <Box>
+      <Text>Select the start and end hours of each day</Text>
+      {days.map((day, index) => {
+        return (
+          <Box key={index}>
+            <Text>{day}</Text>
+            <VStack>
+              <Text>Start time</Text>
+              <Input
+                defaultValue="08:00"
+                type="time"
+                onChange={(e) => {
+                  handleStartTimeChange(day, e.target.value);
+                }}
+              />
+            </VStack>
+            <VStack>
+              <Text>End time</Text>
+              <Input
+                defaultValue="18:00"
+                type="time"
+                onChange={(e) => {
+                  handleEndTimeChange(day, e.target.value);
+                }}
+              />
+            </VStack>
+          </Box>
+        );
+      })}
+      hello world this is the day hours
+      <Button onClick={onPrev}>Previous</Button>
+      <Button
+        onClick={() => {
+          setData({ ...data, startEndTimes: hours });
+          onNext();
+        }}
+      >
+        Continue
+      </Button>
+    </Box>
+  );
+};
+
+export default DayHours;
--- src/components/DaySelection.js
+++ src/components/DaySelection.js
@@ -1,7 +1,59 @@
-import React from "react";
+import { Box, Button, Input } from "@chakra-ui/react";
+import React, { useContext, useEffect, useState } from "react";
+import { DataContext } from "../App";
 
-const DaySelection = () => {
-  return <div>DaySelection</div>;
+const DaySelection = ({ onPrev, onNext }) => {
+  const { data, setData } = useContext(DataContext);
+  const [date, setDate] = useState("");
+  const [finalDates, setFinalDates] = useState([]);
+
+  const formatDateToEnglish = (dateString) => {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
+    const newDate = new Date(dateString + "T00:00:00");
+
+    return newDate.toLocaleDateString("en-US", options);
+  };
+
+  useEffect(() => {
+    console.log(finalDates, " is the data");
+  }, [finalDates]);
+
+  return (
+    <Box>
+      <Input
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+        placeholder="Select Date and Time"
+        size="md"
+        type="date"
+      />
+      <Button
+        onClick={() => {
+          const newArray = [...finalDates, date];
+          setFinalDates(newArray);
+        }}
+      >
+        Select Date
+      </Button>
+      <Button onClick={onPrev}>Previous</Button>
+      <Button
+        onClick={() => {
+          const englishDates = finalDates.map((date) =>
+            formatDateToEnglish(date)
+          );
+          setData({ ...data, daysList: englishDates });
+          onNext();
+        }}
+      >
+        Continue
+      </Button>
+    </Box>
+  );
 };
 
 export default DaySelection;
