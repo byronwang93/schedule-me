e3507c9abdc754a7577ef5527845e007ddcae2e3
Byron Wang <byronwang123919@gmail.com>
2023-10-25 00:11:25 -0700

    started the shifts page

--- src/components/Shifts.js
+++ src/components/Shifts.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { Box, Button } from "@chakra-ui/react";
+import React, { useContext, useEffect } from "react";
+import { DataContext } from "../App";
 
-const Shifts = () => {
-  return <div>Shifts</div>;
+const Shifts = ({ onPrev, onNext }) => {
+  const { data, setData } = useContext(DataContext);
+
+  useEffect(() => {
+    console.log(data, " is the data");
+  }, [data]);
+  return (
+    <Box>
+      <Button onClick={onPrev}>Previous</Button>
+      <Button
+        onClick={() => {
+          onNext();
+        }}
+      >
+        Continue
+      </Button>
+    </Box>
+  );
 };
 
 export default Shifts;
