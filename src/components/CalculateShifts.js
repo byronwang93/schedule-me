28914a6b5d5b423a5b49c862697d2c3461386689
byronwang93 <byronwang123919@gmail.com>
2023-12-26 12:01:00 -0800

    Added loading spinner

--- src/components/CalculateShifts.js
+++ src/components/CalculateShifts.js
@@ -1,4 +1,4 @@
-import { Box, HStack, Spacer, Text, VStack } from "@chakra-ui/react";
+import { Box, HStack, Spacer, Spinner, Text, VStack } from "@chakra-ui/react";
 import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import { DataContext } from "../App";
@@ -8,7 +8,8 @@ import SecondaryButton from "./SecondaryButton";
 const CalculateShifts = ({ onNext, onPrev }) => {
   const { data, setData } = useContext(DataContext);
   const [shifts, setShifts] = useState(null);
-  const [finalShifts, setFinalShifts] = useState(null);
+  const [finalShifts] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     console.log(data, " is the data END END END");
@@ -25,6 +26,7 @@ const CalculateShifts = ({ onNext, onPrev }) => {
   useEffect(() => {
     if (shifts) {
       setData({ ...data, shiftSchedule: shifts });
+      setIsLoading(false);
       onNext();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -37,46 +39,62 @@ const CalculateShifts = ({ onNext, onPrev }) => {
       borderRadius="7px"
       bgColor="#433860"
     >
-      <VStack>
-        <Text pb="30px" alignSelf="baseline" fontSize="20px">
-          Generate my schedule! :')
-        </Text>
-        <HStack w="-webkit-fill-available">
-          <SecondaryButton onClick={onPrev}>Previous</SecondaryButton>
-          <Spacer />
-          <GradientButton
-            onClick={async () => {
-              console.log("clicked");
-              axios
-                .post("http://localhost:3001/make-shifts", data)
-                .then((response) => {
-                  console.log(response, " is the response");
-                  const responseData = response.data.completion.content;
-                  const contentObject = JSON.parse(responseData); // Parse the content string into an object
-                  if (contentObject?.properties?.days) {
-                    const daysObject = contentObject.properties.days;
-                    console.log(daysObject, " is the response we get");
-                    setShifts(daysObject);
-                  } else if (contentObject?.days) {
-                    const daysObject = contentObject.days;
-                    console.log(daysObject, " is the response we get");
-                    setShifts(daysObject);
-                  } else {
-                    throw new Error(
-                      "no valid response w/ this content: ",
-                      contentObject
-                    );
-                  }
-                })
-                .catch((e) => {
-                  console.log(e, " is an error");
-                });
-            }}
-          >
-            Calculate Shifts!
-          </GradientButton>
-        </HStack>
-      </VStack>
+      {isLoading ? (
+        <VStack textAlign="center">
+          <Text className="heading" color="#20FFAF">
+            Loading
+          </Text>
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="#20FFAF"
+            size="xl"
+          />
+        </VStack>
+      ) : (
+        <VStack>
+          <Text pb="30px" alignSelf="baseline" fontSize="20px">
+            Generate my schedule! :')
+          </Text>
+          <HStack w="-webkit-fill-available">
+            <SecondaryButton onClick={onPrev}>Previous</SecondaryButton>
+            <Spacer />
+            <GradientButton
+              onClick={async () => {
+                console.log("clicked");
+                setIsLoading(true);
+                axios
+                  .post("http://localhost:3001/make-shifts", data)
+                  .then((response) => {
+                    console.log(response, " is the response");
+                    const responseData = response.data.completion.content;
+                    const contentObject = JSON.parse(responseData); // Parse the content string into an object
+                    if (contentObject?.properties?.days) {
+                      const daysObject = contentObject.properties.days;
+                      console.log(daysObject, " is the response we get");
+                      setShifts(daysObject);
+                    } else if (contentObject?.days) {
+                      const daysObject = contentObject.days;
+                      console.log(daysObject, " is the response we get");
+                      setShifts(daysObject);
+                    } else {
+                      throw new Error(
+                        "no valid response w/ this content: ",
+                        contentObject
+                      );
+                    }
+                  })
+                  .catch((e) => {
+                    console.log(e, " is an error");
+                  });
+              }}
+            >
+              Calculate Shifts!
+            </GradientButton>
+          </HStack>
+        </VStack>
+      )}
     </Box>
   );
 };
