dd0fd2fa51884fd76e42062ffa700467dcba86ba
byronwang93 <byronwang123919@gmail.com>
2024-02-23 20:55:23 -0800

    initial UI for personal details, still ugly

--- src/components/Shifts.js
+++ src/components/Shifts.js
@@ -1,4 +1,9 @@
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   Box,
   Divider,
   Flex,
@@ -18,9 +23,14 @@ const Shifts = ({ onPrev }) => {
     console.log("final displayed data: ", data);
   }, [data]);
 
-  const { shiftSchedule } = data;
+  const { shiftSchedule, userInfo } = data;
   const [selectedDate, setSelectedDate] = useState(shiftSchedule[0]?.day || 0);
   const [dateDetails, setDateDetails] = useState(null);
+
+  const [selectedPerson, setSelectedPerson] = useState(
+    Object.keys(userInfo)[0] || null
+  );
+  const [personDetails, setPersonDetails] = useState(null);
 
   const formatDateToEnglish = (dateString) => {
     const options = {
@@ -35,6 +45,13 @@ const Shifts = ({ onPrev }) => {
   };
 
   useEffect(() => {
+    const currPersonDetails = userInfo[selectedPerson];
+    console.log(selectedPerson, " is selected person");
+    setPersonDetails(currPersonDetails);
+    console.log(currPersonDetails, " is selected currPersonDetails");
+  }, [selectedPerson, userInfo]);
+
+  useEffect(() => {
     const currDate = shiftSchedule.find((shift) => shift.day === selectedDate);
     setDateDetails(currDate?.shifts);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -45,8 +62,14 @@ const Shifts = ({ onPrev }) => {
     setSelectedDate(event.target.value);
   };
 
+  const handlePersonChange = (event) => {
+    console.log(event.target.value, " is clicked");
+    setSelectedPerson(event.target.value);
+  };
+
   return (
     <Box pt="50px">
+      {/* schedule column */}
       <Flex flexDir="column">
         <Text color="#20FFAF" fontSize="28px" className="heading">
           Schedule
@@ -121,6 +144,130 @@ const Shifts = ({ onPrev }) => {
               })}
           </VStack>
         </VStack>
+      </Flex>
+      {/* indiv person column */}
+      <Flex pt="60px" flexDir="column">
+        <Text color="#20FFAF" fontSize="28px" className="heading">
+          Individual Stats
+        </Text>
+        <Select
+          pt="10px"
+          w={{ base: "240px", md: "270px" }}
+          placeholder="Select date"
+          onChange={handlePersonChange}
+          value={selectedPerson}
+        >
+          {Object.entries(userInfo).map(([name, details]) => {
+            return (
+              <option key={name} value={name}>
+                {name}
+              </option>
+            );
+          })}
+          {/* {userInfo.map((d, index) => {
+            const { day } = d;
+            const updatedDay = formatDateToEnglish(day);
+            return (
+              <option key={index} value={day}>
+                {updatedDay}
+              </option>
+            );
+          })} */}
+        </Select>
+
+        <VStack
+          w={{ base: "360px", md: "680px" }}
+          py="10px"
+          px="15px"
+          borderRadius="5px"
+          bgColor="#433860"
+          alignItems="baseline"
+          overflowY="auto"
+          maxHeight="500px"
+          pt="30px"
+        >
+          <Accordion allowToggle>
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    Scheduled shifts
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                {JSON.stringify(personDetails?.shifts)}
+              </AccordionPanel>
+            </AccordionItem>
+
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    Unavailable but still scheduled during:
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                {JSON.stringify(personDetails?.shiftedUnavailabilities)}
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </VStack>
+        {/* <VStack pt="30px">
+          <HStack alignSelf="baseline" spacing="25px">
+            <Text className="subheading" pl="22px" pr="65px">
+              Shift name
+            </Text>
+            <Text className="subheading" pr="57px">
+              Time
+            </Text>
+            <Text className="subheading">Shift leader</Text>
+            <Text className="subheading">Scheduled</Text>
+          </HStack>
+          <VStack
+            w={{ base: "360px", md: "680px" }}
+            py="10px"
+            px="15px"
+            borderRadius="5px"
+            bgColor="#433860"
+            alignItems="baseline"
+            overflowY="auto"
+            maxHeight="500px"
+          >
+            {dateDetails &&
+              dateDetails.map((shift, index) => {
+                const {
+                  name,
+                  startTime,
+                  endTime,
+                  shiftLeader,
+                  assignedPeople,
+                } = shift;
+                return (
+                  <Box>
+                    <HStack pl="10px" py="10px" key={index}>
+                      <Text w="150px" overflowX="auto">
+                        {name}
+                      </Text>
+                      <Text pl="8px" w="119px">
+                        {startTime}-{endTime}
+                      </Text>
+                      <Text w="98px">{shiftLeader}</Text>
+                      <Text overflowX="auto" w="244px">
+                        {assignedPeople.join(", ")}
+                      </Text>
+                    </HStack>
+                    {index !== dateDetails.length - 1 && (
+                      <Divider ml="1.5%" w="95%" />
+                    )}
+                  </Box>
+                );
+              })}
+          </VStack>
+        </VStack> */}
       </Flex>
       <SecondaryButton mt="40px" px="30px" onClick={onPrev}>
         Calculate Again
